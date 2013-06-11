2e85e514a0bfa0bf1dfe75b722108e17269eaad9
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-06-11 18:09:44 +0100

    Do not put my email in the code.

--- test/test_lwt.ml
+++ test/test_lwt.ml
@@ -4,8 +4,8 @@ let () = match Lwt_main.run
   (sendmail
      ~port:"2525"
      ~name:"localhost"
-     ~from:Addr.(of_string "vb@luminar.eu.org")
-     ~to_:[Addr.(of_string "vb@luminar.eu.org")]
+     ~from:Addr.(of_string "test@example.org")
+     ~to_:[Addr.(of_string "test@example.org")]
      ~body:"Bleh" ()) with
     | `Ok (code, msg) -> Printf.printf "OK %d %s\n" code msg
     | `Failure (code, msg) -> Printf.eprintf "Failure %d %s\n" code msg
