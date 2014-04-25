a270e6f6e3d5b37ae2603c71b2eef3763d0fb553
Vincent Bernardoff <vb@luminar.eu.org>
2014-04-25 13:38:25 +0200

    Install interfaces as well

--- pkg/build.ml
+++ pkg/build.ml
@@ -8,8 +8,8 @@ let () =
   Pkg.describe "smtp" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/smtp";
-    Pkg.lib ~cond:lwt ~exts:Exts.library "src/smtp_lwt";
-    Pkg.lib ~cond:unix ~exts:Exts.library "src/smtp_unix";
+    Pkg.lib ~cond:lwt ~exts:Exts.module_library "src/smtp_lwt";
+    Pkg.lib ~cond:unix ~exts:Exts.module_library "src/smtp_unix";
     Pkg.bin ~cond:lwt ~auto:true ~dst:"smtp_test_lwt" "test/test_lwt";
     Pkg.bin ~cond:unix ~auto:true ~dst:"smtp_test_unix" "test/test_unix";
   ]
